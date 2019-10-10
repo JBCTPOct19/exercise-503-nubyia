83b9f6ae4bcdb694765848190223f46fd77c837b
nubyia <shareenca@gmail.com>
2019-10-10 15:23:47 -0400

    Ex503

---
+++ src/main/java/com/example/demo/Sb503Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb503Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb503Application.class, args);
+    }
+
+}
